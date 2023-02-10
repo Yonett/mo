f36371ea3ec8a9514682fc672c235f86d1343ac4
Yonett <yonett@mail.ru>
2023-02-10 22:18:06 +0700

    golden section done

--- 1/main.py
+++ 1/main.py
@@ -1,5 +1,5 @@
-delta = 1e-11
-eps = 1e-10
+delta = 1e-6
+eps = 1e-5
 
 def func(x):
     return (x-1)**2
@@ -18,14 +18,38 @@ def dichotomy(a, b, func):
         else:
             a = x1
             b = x2
-    print(f"{x1:.10e}, {x2:.10e}")
+    print(f"{x1:.8e}, {x2:.8e}")
+
+def golden_section(a, b, func):
+    x1 = a + 0.381966011*(b-a)
+    x2 = b - 0.381966011*(b-a)
+    value1 = objective(func, x1)
+    value2 = objective(func, x2)
+    while (abs(b-a) > eps):
+        if (value1 < value2):
+            b = x2
+            x2 = x1
+            value2 = value1
+            x1 = a + 0.381966011*(b-a)
+            value1 = objective(func, x1)
+        else:
+            a = x1
+            x1 = x2
+            value1 = value2
+            x2 = b - 0.381966011*(b-a)
+            value2 = objective(func, x2)
+    print(f"{x1:.8e}, {x2:.8e}")
 
 
 def main():
     file = open("funcs.txt")
-    print("Dichotomy:")
     for func in file:
+        print(func, "precision: ", eps)
+        print("Dichotomy:")
         dichotomy(-2, 20, func)
+        print("Golden Section:")
+        golden_section(-2, 20, func)
+        print("")
 
     file.close()
 
