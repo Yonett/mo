8c32962e83435f04ea2588df42375331b1adcfdf
Yonett <yonett@mail.ru>
2023-04-24 00:05:40 +0700

    gauss method done

---
+++ 2/config.py
@@ -0,0 +1,18 @@
+"""
+Params for target function (VARIANT 9)
+"""
+
+A1: int = 2
+A2: int = 3
+
+a1: int = 3
+a2: int = 1
+
+b1: int = 1
+b2: int = 1
+
+c1: int = 2
+c2: int = 1
+
+d1: int = 1
+d2: int = 3
---
+++ 2/singledim.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+def func_test(x: np.array) -> float:
+    return (x[0] - 9) ** 2
+
+def quad_func(x: np.array) -> int:
+    """
+    Quadratic function
+
+    :param x: vector
+
+    :return: function value
+    """
+    return 100 * (x[0, 1] - x[0, 0])**2 + (1 - x[0, 0])**2
+
+def rosen_func(x: np.array) -> float:
+    """
+    Rosenbrock function
+
+    :param x: x-coord
+    :param y: y-coord
+
+    :return: function value
+    """
+    return 100 * (x[0, 1] - x[0, 0]**2)**2 + (1 - x[0, 0])**2
+
+def min_interval(begin: float, func: callable, delta: float, x: np.array, s: np.array):
+    """
+    Finds an interval for search the minimum of a function
+
+    :param begin: start point for searching
+    :param func: callable function
+    :param delta: precision for first iteration
+
+    :return: void
+    """
+    #x0 = 1
+    #s0 = 2
+    iters = h = 0
+    prev = curr = next = begin
+
+    if (func(x + begin * s) > func(x + (begin + delta) * s)):
+        next = begin + delta
+        h = delta
+    if (func(x + begin * s) > func(x + (begin - delta) * s)):
+        next = begin - delta
+        h = -delta
+
+    if (h != 0):
+        while (func(x + curr * s) > func(x + next * s)):
+            iters += 1
+            prev = curr
+            curr = next
+
+            h *= 2
+            next = curr + h
+
+        return prev, next
+    else:
+        return begin - delta, begin + delta
+
+def golden_section(begin: int, end: int, func: callable, eps: float, x: np.array, s: np.array) -> float:
+    """
+    Finds an interval that contains the minimum of a function
+    Using a Golden Section Algorithm
+
+    :param begin: begin of initial interval
+    :param end: end of initial interval
+    :param func: callable function (x, y)
+    :param eps: precision
+
+    :return: void
+    """
+    #x0 = 1
+    #s0 = 2
+    golden_number = 0.381966011
+
+    iters = 0
+
+    left = begin + golden_number * (end - begin)
+    right = end - golden_number * (end - begin)
+
+    value_left = func(x + left * s)
+    value_right = func(x + right * s)
+
+    while (abs(end - begin) > eps):
+        iters += 1
+
+        if (value_left < value_right):
+            end = right
+            right = left
+            value_right = value_left
+            left = begin + golden_number*(end - begin)
+            value_left = func(x + left * s)
+        else:
+            begin = left
+            left = right
+            value_left = value_right
+            right = end - golden_number*(end - begin)
+            value_right = func(x + right * s)
+    
+    return (begin + end) / 2
+
+def main():
+    x = np.array([-25, 34])
+    s = np.matrix([[1, 0], [0, 1]])
+    a = s
+    eps = 1e-7
+
+    for i in range(20000):
+        begin, end = min_interval(0, rosen_func, 1e-10, x, s[0])
+        lambda1 = golden_section(begin, end, rosen_func, 1e-10, x, s[0])
+        #print(lambda1)
+
+        x = x + s[0] * lambda1
+        #print(x)
+
+        begin, end = min_interval(0, rosen_func, 1e-10, x, s[1])
+        lambda2 = golden_section(begin, end, rosen_func, 1e-10, x, s[1])
+        #print(lambda2)
+
+        x = x + s[1] * lambda2
+    print(x)
+
+if __name__ == "__main__":
+    main()
