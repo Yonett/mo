37853907d7ce8b2587011eda196fc4baf317f39d
Yonett <yonett@mail.ru>
2023-05-01 22:05:19 +0700

    tables

--- 2/broyden.py
+++ 2/broyden.py
@@ -7,7 +7,6 @@ funcs_count: int = 0
 
 eps_range = {1e-3, 1e-4, 1e-5, 1e-6, 1e-7}
 
-
 def test_func(x: np.array) -> float:
     global funcs_count
     funcs_count += 1
@@ -39,7 +38,6 @@ def min_interval(begin: float, func: callable, delta: float, x: np.array, s: np.
 
     iters = h = 0
     prev = curr = next = begin
-
     if (func(x + begin * s) > func(x + (begin + delta) * s)):
         next = begin + delta
         h = delta
@@ -102,13 +100,30 @@ def golden_section(begin: int, end: int, func: callable, eps: float, x: np.array
     
     return (begin + end) / 2
 
+
+def gradient(x: np.array, eps: float, func: callable) -> np.array:
+	
+    x = np.asmatrix(x)
+    
+    func_value = func(x)
+
+    result = np.array([0, 0], dtype=float)
+    _x = x
+
+    for i in range(2):
+        _x[0, i] += eps
+        result[i] = (func(_x) - func_value) / eps
+        _x[0, i] -= eps
+
+    return result
+
 def angle(x: np.array, s: np.array):
     return acos((x[0,0]*s[0,0] + x[0,1]*s[0,1]) / (sqrt(x[0,0] ** 2 + x[0,1]**2) * sqrt(s[0,0] ** 2 + s[0,1]**2))) * 180 / 3.1415926535
 
-def rosenbrock(x: np.array, eps: float, target: callable):
-    
-    s = np.matrix([[1, 0], [0, 1]], dtype = float)
-    a = np.matrix([[0, 0], [0, 0]], dtype = float)
+def broyden(x: np.array, eps: float, target: callable):
+    nu = np.matrix([[1, 0], [0, 1]], dtype = float)
+
+    grad = gradient(x, eps, target)
 
     global funcs_count
     funcs_count = 0
@@ -116,63 +131,68 @@ def rosenbrock(x: np.array, eps: float, target: callable):
     deltaF: float = eps
     deltaX: bool = True
     
-    with open(f'rosenbrock/t2/{target.__name__}_{eps:1.1e}.csv', 'w', newline='') as csvfile:
+    with open(f'broyden/t2/{target.__name__}_{eps:1.1e}.csv', 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
         while (deltaF >= eps and deltaX == True):
 
             iters += 1
-            x_prev = np.asmatrix(x)
+            x_prev = x
 
-            begin, end = min_interval(0, target, eps, x, s[0])
-            lambda1: float = golden_section(begin, end, target, eps, x, s[0])
+            if (iters % 2 != 0):
+                nu = np.matrix([[1, 0], [0, 1]], dtype = float)
 
-            x = x + s[0] * lambda1
+            if (np.linalg.det(nu) <= 0):
+                nu = np.matrix([[1, 0], [0, 1]], dtype = float)
 
-            spamwriter.writerow([iters, x[0, 0], x[0, 1], target(x), s[0, 0], s[0, 1], lambda1, abs(x[0, 0] - x_prev[0, 0]), abs(x[0, 1] - x_prev[0, 1]), abs(target(x) - target(x_prev)), angle(x, s[0])])
+            nugrad = np.matmul(nu, grad)
 
-            begin, end = min_interval(0, target, eps, x, s[1])
-            lambda2: float = golden_section(begin, end, target, eps, x, s[1])
+            begin, end = min_interval(0, target, eps, x, nugrad)
+            lambd: float = golden_section(begin, end, target, eps, x, nugrad)
+            
+            dx = lambd * nugrad
+            x = x + dx
 
-            x = x + s[1] * lambda2
+            spamwriter.writerow([iters, np.asmatrix(x)[0, 0], np.asmatrix(x)[0, 1], target(np.asmatrix(x)), nu[0], nu[1], lambd, abs(np.asmatrix(x)[0, 0] - np.asmatrix(x_prev)[0, 0]), abs(np.asmatrix(x)[0, 1] - np.asmatrix(x_prev)[0, 1]), abs(target(np.asmatrix(x)) - target(np.asmatrix(x_prev))), angle(np.asmatrix(x), nugrad), np.asmatrix(grad), nugrad])
 
-            spamwriter.writerow([iters, x[0, 0], x[0, 1], target(x), s[1, 0], s[1, 1], lambda2, abs(x[0, 0] - x_prev[0, 0]), abs(x[0,1] - x_prev[0, 1]), abs(target(x) - target(x_prev)), angle(x, s[1])])
+            _grad = grad
+            grad = gradient(x, eps, target)
 
-            a[0] = lambda1 * s[0] + lambda2 * s[1]
+            dgrad = grad - _grad
 
-            if (abs(lambda1) < abs(lambda2)):
-                a[1] = lambda1 * s[1]
+            tmp1 = dx - np.matmul(nu, dgrad)
+            tmp2 = tmp1.transpose()
+
+            tmp3 = np.matmul(tmp2, tmp1)
+
+            tmp4 = tmp1 * np.asmatrix(dgrad).transpose()
+
+            if (tmp4 == 0):
+                nu = np.matrix([[1, 0], [0, 1]], dtype = float)
             else:
-                a[1] = lambda2 * s[1]
-
-            s[0] = a[0] / np.linalg.norm(a[0])
-
-            b = a[1] - (a[1] * s[0].transpose()) * s[0]
-
-            s[1] = b / np.linalg.norm(b)
+                dnu = tmp3 / tmp4
+                nu = nu + dnu
 
             for i in range(2):
-                if (abs(x[0, i] - x_prev[0, i]) < eps):
+                if (abs(np.asmatrix(x)[0, i] - np.asmatrix(x_prev)[0, i]) < eps):
                     deltaX = False
                     break
 
-            deltaF = abs(target(x) - target(x_prev))
+            deltaF = abs(target(np.asmatrix(x)) - target(np.asmatrix(x_prev)))
 
-
-        return(x, iters, funcs_count)
-
+        return (x, iters, funcs_count)
 
 def main():
     x = np.array([10, -8], dtype = float)
     targets = {test_func, quad_func, rosen_func}
 
     for target in targets:
-        with open(f'rosenbrock/t1/{target.__name__}.csv', 'w', newline='') as csvfile:
+        with open(f'broyden/t1/{target.__name__}.csv', 'w', newline='') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             for eps in eps_range:
-                res, iters, funcs = rosenbrock(x, eps, target)
+                res, iters, funcs = broyden(x, eps, target)
                 spamwriter.writerow([x[0], x[1], eps, iters, funcs, res[0, 0], res[0, 1], target(res)])
-
+    
 
 if __name__ == "__main__":
     main()
